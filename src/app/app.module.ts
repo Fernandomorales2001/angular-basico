2a1d9900273cd79201f7c4af63a1302417ff4d49
Fernando Morales <fmsepeda2001@gmail.com>
2021-07-28 16:56:53 -0600

    Primera Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Heroesmodule } from './heroes/heroes.module';
+import { Contadormodule } from './contador/contado.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Heroesmodule,
+    Contadormodule
   ],
   imports: [
     BrowserModule
